c772d7c105e0b80d60c4e90ce8e0c08ed106fc17
FuzzzzyboySecond <fuzzzzyboy.second@yandex>
2019-08-25 20:42:14 +0300

    codable for transaction

--- TransactionViewer/Models/Transaction.swift
+++ TransactionViewer/Models/Transaction.swift
@@ -7,4 +7,34 @@
 //
 
 struct Transaction {
+
+    var sku: String
+    var currency: Currency
+    var amount: Double
+
+    init(sku: String, currency: Currency, amount: Double) {
+        self.sku = sku
+        self.currency = currency
+        self.amount = amount
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case sku
+        case currency
+        case amount
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        sku = try container.decode(String.self, forKey: .sku)
+        currency = try container.decode(Currency.self, forKey: .currency)
+        let amountString = try container.decode(String.self, forKey: .amount)
+        guard let amount = Double(amountString) else {
+            let context = DecodingError.Context(codingPath: [CodingKeys.amount],
+                                                debugDescription: "Can't convert string to double")
+            throw DecodingError.dataCorrupted(context)
+        }
+        self.amount = amount
+    }
+
 }
